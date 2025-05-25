e3e2ed7e96fa128fd437cfb5fe5ad9894c65cade
Shahriar Refat <shahariarrefat@gmail.com>
2025-05-25 21:03:14 +0600

    weather app - version 2

--- Wather - App/Weather - App V1/script.js
+++ Wather - App/Weather - App V1/script.js
@@ -1,6 +1,5 @@
 const apiKey = "8281290ebd2d9135fbde456fe6030110";
-const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apiUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
